fb744818a295c708e2f2de026e7639800ff90d32
fenil jariwala <feniljariwala82@gmail.com>
2023-06-03 20:51:39 +0530

    auth package reconfigured with api

--- contracts/auth.ts
+++ contracts/auth.ts
@@ -69,5 +69,19 @@ declare module '@ioc:Adonis/Addons/Auth' {
       config: SessionGuardConfig<'user'>
       client: SessionClientContract<'user'>
     }
+    /*
+    |--------------------------------------------------------------------------
+    | OAT Guard
+    |--------------------------------------------------------------------------
+    |
+    | OAT, stands for (Opaque access tokens) guard uses database backed tokens
+    | to authenticate requests.
+    |
+    */
+    api: {
+      implementation: OATGuardContract<'user', 'api'>
+      config: OATGuardConfig<'user'>
+      client: OATClientContract<'user'>
+    }
   }
 }
